0ef382ca189c91b5ba91cc2393426d51560a04db
Andrew Berg <aberg@athenahealth.com>
2019-10-18 14:28:15 -0400

    setup Forge and site shell

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import Root from '@athena/forge/Root';
 import ManageAppointment from './ManageAppointment';
 import Appointments from './Appointments';
 import { ToastContainer } from "react-toastify";
 
+import './App.scss';
+import leaf from './leaf.svg';
+
 function App() {
   return (
-    <>
+    <Root className="ah_app">
       <ToastContainer />
-      <Switch>
-        <Route path="/" exact component={Appointments} />
-        <Route path="/appointments/:day" component={Appointments} />
-        <Route path="/appointments" component={Appointments} />
-        <Route path="/update-appointment/:appointmentId" component={ManageAppointment} />
-        <Route path="/add-appointment/:day" component={ManageAppointment} />
-      </Switch>
-    </>
+      <header className="ah_app--header">
+        <img src={leaf} alt="leaf logo" height="25" className="ah_app--logo"/>
+      </header>
+      <div className="ah_app--body">
+        <aside className="ah_app--navigation"></aside>
+        <main className="ah_app--main">
+          <Switch>
+            <Route path="/" exact component={Appointments} />
+            <Route path="/appointments/:day" component={Appointments} />
+            <Route path="/appointments" component={Appointments} />
+            <Route path="/update-appointment/:appointmentId" component={ManageAppointment} />
+            <Route path="/add-appointment/:day" component={ManageAppointment} />
+          </Switch>
+        </main>
+      </div>
+    </Root>
   );
 }
 
