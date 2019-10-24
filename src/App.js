548a0ce2f85ef5409392a59ada07f3580594ccbd
Andrew Berg <aberg@athenahealth.com>
2019-10-24 10:28:36 -0400

    tweaks to styling and classes

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ import leaf from './leaf.svg';
 function App() {
   return (
     <Root className="ah_app">
-      <header className="ah_app--header">
-        <img src={leaf} alt="leaf logo" height="25" className="ah_app--logo"/>
+      <header className="ah_app__header">
+        <img src={leaf} alt="leaf logo" height="25" className="ah_app__logo"/>
       </header>
-      <div className="ah_app--body">
-        <aside className="ah_app--navigation"></aside>
-        <main className="ah_app--main">
+      <div className="ah_app__body">
+        <aside className="ah_app__navigation"></aside>
+        <main className="ah_app__main">
           <Switch>
             <Route path="/" exact component={Appointments} />
             <Route path="/appointments/:day" component={Appointments} />
